85b7edfe620a75eedc8920f51cae13b0b33b750f
adke29 <adrian.kevin@yahoo.com>
2023-06-02 11:12:58 +0800

    add first frontend

--- src/artflow_frontend/src/components/App.jsx
+++ src/artflow_frontend/src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Item from "./Item"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const NFTID = "aovwi-4maaa-aaaaa-qaagq-cai";
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src="home-img.png" />
+      <Item id = {NFTID}/>
+      {/* <img className="bottom-space" src="home-img.png" /> */}
       <Footer />
     </div>
   );
